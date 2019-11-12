78a57725943837d969b28de72d7a28dd128ea56b
Muhammad Orabi <orabi.dev@gmail.com>
2019-11-13 00:24:22 +0200

    chat module updates && update docker compose env && add reindex in load data script

--- load-data.sh
+++ load-data.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-docker-compose up -d
-sleep 2s
+docker-compose down
+docker-compose up --build -d
+sleep 3s
 docker-compose exec web bash -c "rails db:drop && rails db:create && rails db:migrate"
 docker-compose exec db bash -c "mysql -uroot -proot chat < /dumps/chat_system-09_11_2019.sql"
+echo "searchkick:reindex ...."
+sleep 2s
+docker-compose exec web bash -c "rails searchkick:reindex CLASS=Message"
